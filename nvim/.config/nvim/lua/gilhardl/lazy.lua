c15377fede84fa25663f3521e67e24ef170ed011
Lucas GILHARD <lucas@gilhard.fr>
2025-10-20 20:45:15 +0200

    feat(nvim): add lualine and enable lazy checker

--- nvim/.config/nvim/lua/gilhardl/lazy.lua
+++ nvim/.config/nvim/lua/gilhardl/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("gilhardl.plugins")
+require("lazy").setup("gilhardl.plugins", {
+  checker = {
+    enabled = true
+  }
+})
