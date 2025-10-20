58265ddf748721611c0511e154a9681d6e50f976
Lucas GILHARD <lucas@gilhard.fr>
2025-10-20 19:28:45 +0200

    fix(nvim): telescope config

--- nvim/.config/nvim/lua/gilhardl/plugins/telescope.lua
+++ nvim/.config/nvim/lua/gilhardl/plugins/telescope.lua
@@ -4,34 +4,36 @@ return {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
-    config = function()
-      local telescope = require("telescope")
-      local actions = require("telescope.actions")
-
-      telescope.setup({
-        defaults = {
-          path_display = { "smart" },
-          mapping = {
-            i = {
-              ["<C-k>"] = actions.move_selection_previous,  -- move to prev result
-              ["<C-j>"] = actions.move_selection_next,      -- move to next result
-              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            },
-          },
-          vimgrep_arguments = {
-            'rg',
-            '--color=never',
-            '--no-heading',
-            '--with-filename',
-            '--line-number',
-            '--column',
-            '--smart-case',
-            '--hidden'
-          }
-        },
-      })
-
-      telescope.load_extension("fzf")
-    end
   },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,  -- move to prev result
+            ["<C-j>"] = actions.move_selection_next,      -- move to next result
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
+        },
+      },
+      pickers = {
+        live_grep = {
+          additional_args = function()
+            return { "--hidden" }
+          end,
+        },
+        grep_string = {
+          additional_args = function()
+            return { "--hidden" }
+          end,
+        },
+      },
+    })
+
+    telescope.load_extension("fzf")
+  end
 }
