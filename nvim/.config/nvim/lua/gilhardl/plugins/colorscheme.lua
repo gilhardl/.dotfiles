8b7f59e0a4c1b991eb48acaa883d04c330b63aff
Lucas GILHARD <lucas@gilhard.fr>
2025-10-25 00:29:46 +0200

    feat: use Tokyo Night Storm as dark theme

--- nvim/.config/nvim/lua/gilhardl/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/gilhardl/plugins/colorscheme.lua
@@ -2,6 +2,6 @@ return {
   "folke/tokyonight.nvim",
   priority = 1000, -- top priority to start as first plugin (with auto-dark-mode)
   config = function()
-    vim.cmd("colorscheme tokyonight")
+    vim.cmd("colorscheme tokyonight-storm")
   end
 }
