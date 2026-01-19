9775eb645c54ecfd6f4d5282fe86bbcb81450036
Lucas GILHARD <lucas@gilhard.fr>
2026-01-19 13:07:16 +0100

    fix(homebrew): load shell environment after installation

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,6 +6,9 @@ if ! command -v brew &> /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# Load Homebrew shell environment
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+
 # Install Homebrew packages
 echo "Installing Homebrew packages..."
 brew update && brew bundle install --cleanup --file=~/.dotfiles/homebrew/Brewfile && brew upgrade
